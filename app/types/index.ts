2145097eb00466cd237b1aa4dcb06c57199e660c
Imam Teguh <imam.teguh33@gmail.com>
2025-11-18 20:52:07 +0700

    feat: tambahkan dashboard layout

--- app/types/index.ts
+++ app/types/index.ts
@@ -1,8 +1,24 @@
 export interface User {
-    _id: string
-    email: string
-    password: string
-    name: string
-    createdAt: Date
-    updatedAt: Date
+  _id: string;
+  email: string;
+  name: string;
+  createdAt: Date;
+  isEmailVerified: boolean;
+  updatedAt: Date;
+  profilePicture?: string;
+}
+
+export interface Workspace {
+  _id: string;
+  name: string;
+  description?: string;
+  owner: User | string;
+  color: string;
+  members: {
+    user: User;
+    role: "admin" | "member" | "owner" | "viewer";
+    joinedAt: Date;
+  }[];
+  createdAt: Date;
+  updatedAt: Date;
 }
